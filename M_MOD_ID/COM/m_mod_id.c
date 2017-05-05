a9fd4ffedb777ee3b88d37e0463e8bcf54c74f80
thomas.schnuerer <thomas.schnuerer@men>
2017-05-05 14:48:52 +0200

    added GPL license notification under every MEN document header

--- M_MOD_ID/COM/m_mod_id.c
+++ M_MOD_ID/COM/m_mod_id.c
@@ -46,8 +46,22 @@
  * Added by mcvs
  *
  *---------------------------------------------------------------------------
- * (c) Copyright 1998..2009 by MEN mikro elektronik GmbH, Nuernberg, Germany 
- ****************************************************************************/
+ * (c) Copyright 1998..2009 by MEN mikro elektronik GmbH, Nuremberg, Germany 
+ ******************************************************************************/
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
  
 static const char RCSid[]="$Id: m_mod_id.c,v 2.4 2009/09/23 13:02:45 CRuff Exp $";
 
